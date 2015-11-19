fa72daae13c59006ade7eec8f0907ca2128de3f0
Gasper Andrejc <ga6289@student.uni-lj.si>
2015-11-19 19:48:20 +0100

    table filtering for user's dashboard

--- js/main.js
+++ js/main.js
@@ -2,47 +2,56 @@
  * Created by gandrejc on 18.11..
  */
 var EXPENSE = {
-    init: function (){
+    init: function () {
         EXPENSE.closePopup();
         EXPENSE.openPopup();
         EXPENSE.handleTableOpenClose();
         EXPENSE.userInteractionHandler();
-
+        EXPENSE.tableFiltering();
 
 
     },
-    openPopup: function(){
-        $(".open-connected-orders").click(function(){
+    openPopup: function () {
+        $(".open-connected-orders").click(function () {
             $("#connected-order").load("connected-order.html");
             $("#connected-order").toggleClass("hidden");
 
         });
-        $(".open-order").click(function(){
+        $(".open-request").click(function () {
+            $("#open-request-popup").load("../view-request.html");
+            $("#open-request-popup").toggleClass("hidden");
+
+        });
+        $(".open-order").click(function () {
             $("#connected-order").load("../view-order.html");
             $("#connected-order").toggleClass("hidden");
         });
-        $(".open-connected").click(function(){
+        $(".open-connected").click(function () {
             $("#connected").toggleClass("hidden");
         });
 
-        $("#new-order").click(function(){
+        $("#new-order").click(function () {
             $("#add-new-popup").toggleClass("hidden");
         });
     },
-    closePopup: function(){
-        $(".close-popup").click(function(){
+    closePopup: function () {
+        $(".close-popup").click(function () {
             $(this).closest(".background-popup").toggleClass("hidden");
         });
         //for those that are dynamically added from another file
-        $("#connected-order").on("click", ".glyphicon-remove-sign", function(){
+        $("#connected-order").on("click", ".glyphicon-remove-sign", function () {
             $(this).closest(".background-popup").toggleClass("hidden");
             $("#connected-order").empty();
         });
+        $("#open-request-popup").on("click", ".glyphicon-remove-sign", function () {
+            $(this).closest(".background-popup").toggleClass("hidden");
+            $("#open-request-popup").empty();
+        });
     },
-    handleTableOpenClose: function() {
+    handleTableOpenClose: function () {
         $("tr[class*='collapsable']").next().slideToggle();
 
-        $("tr").click(function(event) {
+        $("tr").click(function (event) {
             var $target = $(event.target);
             var closesTr = $target.closest("tr[class*='collapsable']");
             closesTr.toggleClass("info");
@@ -53,7 +62,7 @@ var EXPENSE = {
 
         //for those that are dynamicall added from file
 
-        $("#connected-order").on("click", "tr", function(){
+        $("#connected-order").on("click", "tr", function () {
             var $target = $(event.target);
             var closesTr = $target.closest("tr[class*='collapsable']");
             closesTr.toggleClass("info");
@@ -61,39 +70,85 @@ var EXPENSE = {
             closesTr.next().slideToggle();
         });
     },
-    userInteractionHandler: function() {
-            $("#addNewService").click(function(){
-                var table = $("#orderServices");
-                var inputService = "<input type='text' class='tableInput serviceInput' placeholder='Tip storitve'</input>";
-                var inputPrice = "<input type='number' class='tableInput priceInput' placeholder='Cena'</input>";
-                var inputNotes = "<input type='text' class='noteInput' placeholder='Opombe'</input>";
-                var addBtn = '<button type="button" class="btn-xs btn-success glyphicon glyphicon-plus float-right insertService"></button>'
-                var inputFieldsHtml = "<tr id='addingEntry'> <td>"+inputService+"</td> <td>"+inputPrice+"</td> <td>"+inputNotes+addBtn+"</td> </tr>";
-                table.append(inputFieldsHtml);
+    userInteractionHandler: function () {
+        $("#addNewService").click(function () {
+            var table = $("#orderServices");
+            var inputService = "<input type='text' class='tableInput serviceInput' placeholder='Tip storitve'</input>";
+            var inputPrice = "<input type='number' class='tableInput priceInput' placeholder='Cena'</input>";
+            var inputNotes = "<input type='text' class='noteInput' placeholder='Opombe'</input>";
+            var addBtn = '<button type="button" class="btn-xs btn-success glyphicon glyphicon-plus float-right insertService"></button>'
+            var inputFieldsHtml = "<tr id='addingEntry'> <td>" + inputService + "</td> <td>" + inputPrice + "</td> <td>" + inputNotes + addBtn + "</td> </tr>";
+            table.append(inputFieldsHtml);
+        });
+        $("#orderServices").on("click", ".insertService", function () {
+            var table = $("#orderServices");
+            var service = $(".serviceInput").val();
+            var price = $(".priceInput").val();
+            var note = $(".noteInput").val();
+            $("#addingEntry").remove();
+            var htmlEntry = '<tr> <td>' + service + '</td> <td>' + price + '</td> <td>' + note + '</td> </tr>'
+            table.append(htmlEntry);
+        });
+        $("#editOrder").click(function () {
+            $(this).toggleClass("disabled");
+            var addButtonTo = $("#orderServices").parent(".content-table.section");
+
+            $("#orderServices").find("td").each(function () {
+                var value = $(this).html();
+                if (isNaN(value)) {
+                    $(this).html('<input type="text" class="tableInput serviceInput adding" value="' + value + '"></input>');
+                } else {
+                    $(this).html('<input type="number" class="tableInput priceInput adding" value="' + value + '"></input>');
+                }
             });
-            $("#orderServices").on("click", ".insertService", function(){
-                var table = $("#orderServices");
-                var service = $(".serviceInput").val();
-                var price = $(".priceInput").val();
-                var note = $(".noteInput").val();
-                $("#addingEntry").remove();
-                var htmlEntry = '<tr> <td>'+service+'</td> <td>'+price+'</td> <td>'+note+'</td> </tr>'
-                table.append(htmlEntry);
-            })
 
+            addButtonTo.append('<button type="button" class="btn-xs btn-info glyphicon glyphicon-ok completeEdit float-right"></button>');
+        });
+        $("#orderServices").parent(".content-table.section").on("click", ".completeEdit", function(){
+            $("#orderServices").find(".adding").each(function () {
+                var value = $(this).val();
+                $(this).parent().html('<td>'+value+'</td>');
+            });
+            $(".completeEdit").toggleClass("hidden")
+        });
+        $(".trash-request").click(function(){
+           $(this).closest("tr").empty();
+        });
+    },
+    tableFiltering: function(){
+        //user dashboard status table filtering
+        $(".request-filter-user").click(function(){
+            //get values
+            var allFilters = $(this).parent().parent().find("input[class*='request-filter-user']");
+            var zero = $(allFilters[0]).is(":checked");
+            var one = $(allFilters[1]).is(":checked");
+            var two = $(allFilters[2]).is(":checked");
 
+            $(".section.status table tbody tr").each(function(){
+               if($(this).attr("status-data") == 0){
+                    zero ? $(this).removeClass("hidden"):$(this).addClass("hidden");
+               }
+               else if($(this).attr("status-data") == 1){
+                    one ? $(this).removeClass("hidden"):$(this).addClass("hidden");
+               }else{
+                   two ? $(this).removeClass("hidden"):$(this).addClass("hidden");
+               }
+            });
+
+        });
+        //my offers user
 
     }
 };
 
 /* RUN ON READY */
-$(document).ready(function() {
+$(document).ready(function () {
 
     EXPENSE.init();
 
 });
 
 /* RUN WINDOW LOAD */
-$(document).load(function() {
+$(document).load(function () {
 
 });
